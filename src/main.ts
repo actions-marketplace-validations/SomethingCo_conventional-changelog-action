cdd370cdeaf5f4e5ecf0d3bfd739c5c7717ef546
Sallar Kaboli <sallar@kaboli.org>
2021-04-28 22:43:09 +0300

    feat: implement action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,117 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { DateTime } from 'luxon'
+import parser from 'conventional-commits-parser'
+import groupBy from 'lodash/groupBy'
+
+const emojis: Record<string, string> = {
+  feat: `:sparkles:`,
+  fix: `:bug:`,
+  docs: `:books:`,
+  style: `:gem:`,
+  refactor: `:hammer:`,
+  perf: `:rocket:`,
+  test: `:rotating_light:`,
+  build: `:package:`,
+  ci: `:construction_worker:`,
+  chore: `:wrench:`
+}
+
+const titles: Record<string, string> = {
+  feat: `Features`,
+  fix: `Bug Fixes`,
+  docs: `Docs`,
+  style: `Styling`,
+  refactor: `Refactor`,
+  perf: `Performance`,
+  test: `Tests`,
+  build: `Build`,
+  ci: `CI`,
+  chore: `Chores`
+}
+
+function createChangelog(
+  commits: parser.Commit<string | number | symbol>[]
+): string {
+  const grouped = groupBy(commits, commit => commit.type)
+
+  return Object.keys(grouped)
+    .map(type => {
+      const title = titles[type] ?? 'Others'
+      const emoji = emojis[type] ?? ':question:'
+      const commitsList = grouped[type]
+        .map(commit => `- ${commit.subject || commit.header}`)
+        .join('\n')
+
+      return `### ${emoji} ${title}:\n${commitsList}`
+    })
+    .join(`\n\n`)
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const repoName = github.context.payload.repository?.name
+    const tagName = github.context.payload.release?.tag_name
+    const releaseId = github.context.payload.release?.id
+    const owner = github.context.payload.repository?.owner.login
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { GITHUB_TOKEN } = process.env
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!repoName || !tagName || !owner || !releaseId) {
+      throw new Error('Only possible to run this action in a release event')
+    }
+
+    if (!GITHUB_TOKEN) {
+      throw new Error('GITHUB_TOKEN is required.')
+    }
+
+    const octokit = github.getOctokit(GITHUB_TOKEN)
+    const allReleases = await octokit.request(
+      `GET /repos/{owner}/{repo}/releases`,
+      {
+        owner,
+        repo: repoName,
+        per_page: 15
+      }
+    )
+
+    if (allReleases.data.length < 2) {
+      core.debug('First release. Not really possible to create a changelog.')
+      return
+    }
+
+    const sortedReleases = allReleases.data.sort(
+      (a, b) =>
+        DateTime.fromISO(b.created_at).toMillis() -
+        DateTime.fromISO(a.created_at).toMillis()
+    )
+
+    core.debug(`${sortedReleases[0].id}`)
+    const tagNameToCompare = sortedReleases[1].tag_name
+
+    const commitsResponse = await octokit.request(
+      `GET /repos/{owner}/{repo}/compare/{base}...{head}`,
+      {
+        owner,
+        repo: repoName,
+        base: tagNameToCompare,
+        head: tagName
+      }
+    )
+
+    const mappedCommits = commitsResponse.data.commits.map(data =>
+      parser.sync(data.commit.message)
+    )
+
+    const changelogBody = createChangelog(mappedCommits)
+    await octokit.request(`PATCH /repos/{owner}/{repo}/releases/{release_id}`, {
+      repo: repoName,
+      owner,
+      release_id: releaseId,
+      body: changelogBody
+    })
+
+    core.setOutput('changelog', changelogBody)
   } catch (error) {
     core.setFailed(error.message)
   }
